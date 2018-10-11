2dfba157e289071a514731e1483217c937e2f147
Gaurav Gupta <gouravgupta840@gmail.com>
2018-10-12 04:20:38 +0530

    Detail Rental view component added and used childRoute
    aka Nested Route

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RentalListComponent } from './rental-list/rental-list.component';
-import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
+import { RentalListItemComponent } from './rental-list/rental-list-item/rental-list-item.component';
 import { RentalComponent } from './rental.component';
 import { CommonModule } from '@angular/common';
 import { RentalService } from './Shared/rental.service';
+
+import { Routes, RouterModule } from '@angular/router';
+import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+
+const routes: Routes = [
+    {path: 'rentals', component: RentalComponent , children : [
+        {path : '', component: RentalListComponent},
+        {path : ':rentalId', component: RentalDetailComponent}
+    ]},
+  ]
 
 @NgModule({
     declarations:[
         RentalListComponent,
         RentalListItemComponent,
-        RentalComponent
+        RentalComponent,
+        RentalDetailComponent
     ],
     imports:[
-    CommonModule
+     CommonModule,
+     RouterModule.forChild(routes),
     ],
     providers:[RentalService]
 })
