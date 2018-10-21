3c34e2c9ddf707a56852d5c9b69f1247b1039802
Gaurav Gupta <gouravgupta840@gmail.com>
2018-10-21 20:14:24 +0530

    Auth Middleware Created Half

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,29 @@
 const User = require('../models/user');
 const {normalizeErrors} = require('../helper/mongoose');
 exports.auth = function (req,res) {
-    
+    const {  email, password } = req.body;
+
+    if(!password || !email){
+        return res.status(422).send({errors: [{title: 'Data missing', detail: 'Provide email and password!'}]});
+    }
+
+    User.findOne({email} , function(err,user){
+        if(err) {
+            return res.status(422).send({errors: normalizeErrors(err.errors)});
+        }
+
+        if(!user) {
+            return res.status(422).send({errors:[{title: 'Invalid User!', detail: 'User does not exist'}]});
+        }
+
+        if(user.isSamePassword(password)){
+
+        }else {
+            return res.status(422).send({errors:[{title: 'Wrong Detail', detail: 'Wrong user or password'}]});
+        }        
+        
+    });
+
 }
 
 exports.register = function(req, res) {
