3e8a9f9524028d0d45b63e117c9e81ae20f0432e
Gaurav Gupta <gouravgupta840@gmail.com>
2018-10-23 02:37:12 +0530

    Login User

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from './../shared/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bwm-login',
@@ -9,8 +11,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  errors:any [] = [];
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.initForm();
@@ -44,7 +47,15 @@ export class LoginComponent implements OnInit {
   }
 
   Login(){
-    console.log(this.loginForm.value);
+    // console.log(this.loginForm.value);
+    this.auth.login(this.loginForm.value).subscribe(
+      (token) => {
+        this.router.navigate(['/rentals']);
+      },
+      (errorResponse) => {
+        this.errors = errorResponse.error.errors;
+      }
+    );
   }
 
 }
--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class AuthService {
@@ -10,5 +11,20 @@ export class AuthService {
     public register(userData: any): Observable<any>{
         return this.http.post('/api/v1/users/register', userData);
     }
+
+    public login(userData: any): Observable<any> {
+        return this.http.post('/api/v1/users/auth', userData)
+        .map((token) => this.setToken(token));
+    }
+
+    public setToken(token): string {
+        localStorage.setItem('bwm_auth',token);
+        return token;
+    }
+
+    // public logOut() {
+    //     var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    //     var token = currentUser.token; // your token
+    // }
 
 }
