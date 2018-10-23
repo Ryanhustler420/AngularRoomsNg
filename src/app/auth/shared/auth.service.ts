da7c409e0d0e1b7566e947f6a90286a802a054c1
Gaurav Gupta <gouravgupta840@gmail.com>
2018-10-23 13:08:02 +0530

    Authentication Implimentation

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import * as jwt from 'jsonwebtoken';
+import * as moment from 'moment';
 import 'rxjs/Rx';
 
 class DecodedToken {
@@ -14,7 +15,20 @@ export class AuthService {
 
     private decodedToken: DecodedToken;
 
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient){
+        this.decodedToken = JSON.parse(localStorage.getItem('bwm_meta')) || new DecodedToken();
+    }
+    
+    private setToken(token): string {
+        this.decodedToken = jwt.decode(token);
+        localStorage.setItem('bwm_auth',token);
+        localStorage.setItem('bwm_meta', JSON.stringify(this.decodedToken));
+        return token;
+    }
+
+    private getExpiration() {
+        return moment.unix(this.decodedToken.exp);
+    }
 
     public register(userData: any): Observable<any>{
         return this.http.post('/api/v1/users/register', userData);
@@ -25,12 +39,8 @@ export class AuthService {
         .map((token) => this.setToken(token));
     }
 
-    public setToken(token): string {
-        this.decodedToken = jwt.decode(token);
-        localStorage.setItem('bwm_auth',token);
-        debugger;
-        localStorage.setItem('bwm_meta', JSON.stringify(this.decodedToken));
-        return token;
+    public isAuthenticated() : boolean {
+        return moment().isBefore(this.getExpiration());
     }
 
     // public logOut() {
--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './../../auth/shared/auth.service';
 
 @Component({
     selector:"bwm-header",
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
 
 export class HeaderComponent {
 
+    constructor(private auth: AuthService){}
+    
 }
