4a916f85f2bf261ca8fc2eae5b3c27c849e26a4a
Gaurav Gupta <gouravgupta840@gmail.com>
2018-10-28 21:41:08 +0530

    Get Booking Data part 1

--- src/app/common/service/helper.service.ts
+++ src/app/common/service/helper.service.ts
@@ -5,20 +5,32 @@ import { Booking } from './../../booking/shared/booking.model';
 @Injectable()
 export class HelperService {
 
-    public getRangeDates(startAt, endAt){
+    private getRangeDates(startAt, endAt, format){
         const tempDates = [];
         const mEndAt = moment(endAt);
         let mStartAt = moment(startAt);
 
         while(mStartAt < mEndAt){
-            tempDates.push(mStartAt.format(Booking.DATE_FORMAT));
+            tempDates.push(mStartAt.format(format));
             mStartAt = mStartAt.add(1, 'day');
         }
 
-        tempDates.push(moment(startAt).format(Booking.DATE_FORMAT));
-        tempDates.push(mEndAt.format(Booking.DATE_FORMAT));
+        tempDates.push(moment(startAt).format(format));
+        tempDates.push(mEndAt.format(format));
 
         return tempDates;
+    }
+
+    public formatDate(date, dateformat) {
+        return moment(date).format(dateformat);
+    }
+
+    public formatBookingDate(date){
+        return this.formatDate(date,Booking.DATE_FORMAT);
+    }
+
+    public getBookingRangeOfDate(startAt, endAt){
+        return this.getRangeDates(startAt, endAt, Booking.DATE_FORMAT);
     }
 
 }
--- src/app/rental/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
+++ src/app/rental/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
@@ -12,6 +12,7 @@ export class RentalDetailBookingComponent implements OnInit {
 
   @Input() price : number;
   @Input() bookings : Booking[];
+  newBooking: Booking;
 
   public daterange: any = {};
   public bookedOutDates: any[] = [];
@@ -29,24 +30,31 @@ export class RentalDetailBookingComponent implements OnInit {
   
   ngOnInit() {
     this.getBookedOutDates();
+    this.newBooking = new Booking();
   }
 
   private checkForInvalidDates(date){
-    return this.bookedOutDates.includes(date.format(Booking.DATE_FORMAT)) || date.diff(moment(), 'days') < 0;
+    return this.bookedOutDates.includes(this.helper.formatBookingDate(date)) || date.diff(moment(), 'days') < 0;
   }
 
   private getBookedOutDates() {
     if(this.bookings && this.bookings.length > 0){
       this.bookings.forEach((booking: Booking) => {
-        const dateRange =  this.helper.getRangeDates(booking.startAt,booking.endAt);
+        const dateRange =  this.helper.getBookingRangeOfDate(booking.startAt,booking.endAt);
         this.bookedOutDates.push(...dateRange);
       });
     }
   }
 
   public selectedDate(value: any, datepicker?: any) {
-    datepicker.start = value.start;
-    datepicker.end = value.end;
+    // datepicker.start = value.start;
+    // datepicker.end = value.end;
+
+    this.newBooking.startAt = this.helper.formatBookingDate(value.start);
+    this.newBooking.endAt = this.helper.formatBookingDate(value.end);
+    this.newBooking.days = -(value.start.diff(value.end, 'days'));
+
+    console.log(this.newBooking);
     
     this.daterange.start = value.start;
     this.daterange.end = value.end;
