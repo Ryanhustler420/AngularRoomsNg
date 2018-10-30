761721d26b6046f7aa71049cfaa081f219980bb6
Gaurav Gupta <gouravgupta840@gmail.com>
2018-10-30 20:21:15 +0530

    Search city endpoint start

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Rental = require('../models/rental');
 const UserCtrl = require('../controllers/user');
+const { normalizeErrors } = require('../helper/mongoose');
 
 router.get('/secret' , UserCtrl.authMiddleware, function(req,res){
     res.json({"secret":true})
@@ -27,7 +28,18 @@ router.get('',(req,res) => {
     const city = req.query.city;
 
     if(city){
-        return res.json({city});
+
+        Rental.find({city: city.toLowerCase()})
+            .select('-bookings')
+            .exec(function(err, filterRentals){
+                if(err){
+                    return res.status(422).send({errors: normalizeErrors(err.errors)});
+                }
+                if(filterRentals.length == 0){
+                    return res.status(422).send({error:[{title:'No Rental Found!', detail: `There are no rentals for city ${city}`}]});
+                }
+                return res.json(filterRentals);
+            })
     }else{
         Rental.find({})
         .select('-bookings')
