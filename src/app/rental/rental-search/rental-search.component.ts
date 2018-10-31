63f7cf9ea94af660169efa31db3ec06fef3e4989
Gaurav Gupta <gouravgupta840@gmail.com>
2018-10-31 14:23:19 +0530

    Handle Search Errors adn other Edge Cases

--- src/app/rental/rental-search/rental-search.component.ts
+++ src/app/rental/rental-search/rental-search.component.ts
@@ -3,6 +3,7 @@ import { RentalService } from './../Shared/rental.service';
 import { Rental } from './../Shared/rental.model';
 import { ActivatedRoute } from '@angular/router';
 
+import { HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'bwm-rental-search',
   templateUrl: './rental-search.component.html',
@@ -12,6 +13,7 @@ export class RentalSearchComponent implements OnInit {
 
   rentals: Rental[] = [];
   city: string;
+  errors: any[] = [];
 
   constructor(private rentalService:RentalService, private activateRoute: ActivatedRoute) { }
   
@@ -26,7 +28,7 @@ export class RentalSearchComponent implements OnInit {
     const rentalObservable = this.rentalService.getRentalsWithCity(this.city)
     rentalObservable.subscribe(
         (rentals: Rental[]) => { this.rentals = rentals; },
-        (err) => {},
+        (err: HttpErrorResponse) => { this.errors = err.error.error },
         () => {} 
     );
   }
